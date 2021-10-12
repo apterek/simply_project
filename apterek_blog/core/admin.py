33cdee1da6f7e9b39ef0e92889279114a4550e52
apterek <danialsud@gmail.com>
2021-10-12 12:56:06 +0300

    remake project

---
+++ apterek_blog/core/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from core.models import Post, Categories, Comments, Tag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "author", "created_at", )
+    search_fields = ("title", "author", "created_at", )
+    readonly_fields = ("created_at", "number_of_views", )
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ("category", "sub_category", )
+    search_fields = ("category", "sub_category", )
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("posts", "created_at",)
+    search_fields = ("posts", )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("title", )
+    search_fields = ("posts", )
---
+++ apterek_blog/core/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey("Categories", on_delete=models.CASCADE)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name="profiles")
+    post_image = models.ImageField(upload_to='post_images/%Y/%m/%d/')
+    number_of_views = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return f"{self.title} - {self.created_at}"
+
+
+class Categories(models.Model):
+    category = models.CharField(max_length=255)
+    sub_category = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.category}/{self.sub_category}"
+
+
+class Comments(models.Model):
+    name = models.CharField(max_length=50, default="Noname")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey("self",
+                               verbose_name="parent_comment",
+                               on_delete=models.SET_NULL,
+                               blank=True, null=True)
+    posts = models.ForeignKey(Post,
+                              related_name="comment",
+                              on_delete=models.CASCADE,
+                              verbose_name="comment_to_post",
+                              default="")
+
+    class Meta:
+        verbose_name = "Comments"
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return f"{self.created_at}"
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=10, unique=True)
+    posts = models.ManyToManyField(Post, related_name="tags")
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def __str__(self):
+        return self.title
