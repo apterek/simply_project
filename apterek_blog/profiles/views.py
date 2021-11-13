0e7f5390a26845e697b7dd5ac345c24071666a05
apterek <danialsud@gmail.com>
2021-11-13 15:48:00 +0300

    Up

--- apterek_blog/profiles/views.py
+++ apterek_blog/profiles/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
 from profiles.forms import EditProfileInfoForm
 from profiles.models import ProfileInformation
 
@@ -10,3 +10,12 @@ class ProfileView(TemplateView):
         if self.request.user.is_authenticated:
             profile = ProfileInformation.objects.all().filter(user_id=self.request.user.id).first()
             return {"profile": profile}
+
+
+class UpdateProfile(UpdateView):
+    template_name = "profile/update_profile_page.html"
+    form_class = EditProfileInfoForm
+    success_url = "profile_url"
+
+
+
