42352f289bd7b801507bed614cecfb74e7235b50
apterek <danialsud@gmail.com>
2021-11-13 14:27:39 +0300

    add admin profile

--- apterek_blog/profiles/admin.py
+++ apterek_blog/profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from profiles.models import ProfileInformation
 
-# Register your models here.
+
+@admin.register(ProfileInformation)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "bday", "gender", "post", "about", )
+    search_fields = ("user", "name", "post", )
+
