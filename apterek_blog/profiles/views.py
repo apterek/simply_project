3f30ddec2fb53d46b422586ad058f6547d7a2790
apterek <danialsud@gmail.com>
2021-11-13 14:25:21 +0300

    add profile view

--- apterek_blog/profiles/views.py
+++ apterek_blog/profiles/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView, FormView, UpdateView, DetailView
+from django.views.generic import TemplateView
 from profiles.forms import EditProfileInfoForm
+from profiles.models import ProfileInformation
 
 
 class ProfileView(TemplateView):
+    template_name = "profile/user_profile_page.html"
 
-
-
+    def get_context_data(self, **kwargs):
+        if self.request.user.is_authenticated:
+            profile = ProfileInformation.objects.filter(user_id=self.request.user.id)
+            return {"profile": profile}
